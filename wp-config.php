5b6fec874a90a1ef819bcc3e41ef764c226c43af
hostoctopus <33010144+hostoctopus@users.noreply.github.com>
2017-11-09 14:48:50 -0900

    Disable Updates on Test and Live (on), Disable File Edits (place holder)
    Line 189: define('DISALLOW_FILE_EDIT', false); Make true when live and public.

--- wp-config.php
+++ wp-config.php
@@ -186,7 +186,14 @@ define('FTP_HOST', 'appserver.dev.294ef13b-41af-4b74-abf4-2ef875fdc8a7.drush.in'
 define('FTP_USER', 'dev.294ef13b-41af-4b74-abf4-2ef875fdc8a7');
 define('FTP_PASS', '9a:29:2a:00:75:19:d7:17:e6:26:5c:04:0b:1f:ff:ad');
 define('FTP_SSL', true);
+define('DISALLOW_FILE_EDIT', false);
+// FS writes aren't permitted in test or live, so we should let WordPress know to disable relevant UI
 
+   if ( in_array( $_ENV['PANTHEON_ENVIRONMENT'], array( 'test', 'live' ) ) && ! defined( 'DISALLOW_FILE_MODS' ) ) :
+
+       define( 'DISALLOW_FILE_MODS', true );
+
+   endif;
 
 
 /** Absolute path to the WordPress directory. */
