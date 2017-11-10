7512a2d9934a6af0cefac89043c2a48c0c927bec
hostoctopus <33010144+hostoctopus@users.noreply.github.com>
2017-11-09 22:58:01 -0900

    Allow file edit (for now)
    "//<<REMOVE"//" to enable this line"

--- wp-config.php
+++ wp-config.php
@@ -389,5 +389,5 @@ require_once(ABSPATH . 'wp-settings.php');
 # END iThemes Security - Do not modify or remove this line
 // BEGIN iThemes Security - Do not modify or remove this line
 // iThemes Security Config Details: 2
-define( 'DISALLOW_FILE_EDIT', true ); // Disable File Editor - Security > Settings > WordPress Tweaks > File Editor
+//<<REMOVE"//" to enable this line define( 'DISALLOW_FILE_EDIT', true ); // Disable File Editor - Security > Settings > WordPress Tweaks > File Editor
 // END iThemes Security - Do not modify or remove this line
